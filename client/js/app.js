0bb7b6611cfbf6df8b52fab86acd340e967a3489
Matt Ashbourne <matt.ashbourne@gmail.com>
2014-03-15 16:15:39 -0400

    fixed widget visual options, removed plangular lib

--- client/js/app.js
+++ client/js/app.js
@@ -15,26 +15,20 @@ app.controller("PlayerCtrl", function ($scope, $firebase) {
 		
 		var newSoundUrl = dataSnapshot.val().cur;//'http://api.soundcloud.com/tracks/' + value.cur;
 
-		widget.load(newSoundUrl);
+		widget.load(newSoundUrl, 
+		{ 
+			auto_play: true,
+			show_artwork: true,
+			visual: true,
+			buying: false,
+			liking: false,
+			download: false,
+			sharing: false,
+			show_comments: false,
+			show_playcount: false,
+			show_user: false
+		});
 	});
-
-	widget.bind(SC.Widget.Events.READY, function() {
-      // load new widget
-      widget.bind(SC.Widget.Events.FINISH, function() {
-        widget.load(newSoundUrl, {
-          auto_play: true
-        });
-      });
-    });
-  // 	$scope.nowplaying.$on('loaded', function(value) {
-		// console.log(value); // data loaded from Firebase
-		// console.log(value.cur); // subset of the returned value
-		// var widgetIframe = document.getElementById('sc-widget');
-		// var widget       = SC.Widget(widgetIframe);
-		// var newSoundUrl = value.cur;//'http://api.soundcloud.com/tracks/' + value.cur;
-
-		// widget.load(newSoundUrl);
-  //   });
 
 
 }
