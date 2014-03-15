2ef359d1015a1be9f78aae686a91ce4822789d05
George Lifchits <george.lifchits@gmail.com>
2014-03-15 14:28:11 -0400

    change song working nicer

--- server/queue.py
+++ server/queue.py
@@ -18,32 +18,26 @@ class SongQueue:
 
     def get_previous_queue(self):
         ''' returns the previously played queue '''
-        result = firebase.get(config.QUEUE, config.PREV)
-        return result
+        queue = firebase.get(config.QUEUE, config.PREV) or []
+        return queue
 
     def change_songs(self):
         ''' puts current song in previous queue, sets next song to current '''
         prev_queue = self.get_previous_queue() # working copy of prev queue
-        prev_queue.pop() # pop last song off the back
         prev_queue.insert(0, self.get_current_song()) # current song at front
         result = firebase.put(config.QUEUE, config.PREV, prev_queue) # update firebase
 
-        new_id = self.get_next_song()
-        result = firebase.put(config.QUEUE, config.CURRENT, int(new_id))
+        next_queue = firebase.get(config.QUEUE, config.NEXT)
+        if next_queue is None:
+            next_id = self._get_auto_song()
+        else:
+            next_id = next_queue.pop(0) # get current song off the front
+            result = firebase.put(config.QUEUE, config.NEXT, next_queue)
+
+        result = firebase.put(config.QUEUE, config.CURRENT, int(next_id))
         return result
 
-    def get_next_song(self):
-        ''' returns the song ID to be played next '''
-        queue = firebase.get(config.QUEUE, config.NEXT)
-        next_id = None
-        if len(queue) > 0: # if the queue has songs
-            next_id = queue[0] # get song at front of queue
-        else:
-            next_id = self._auto_add_song()
-            pass
-        return next_id
-
-    def _auto_add_song():
+    def _get_auto_song(self):
         ''' uses some neat algorithm to pick the next song '''
         songs = self._songs.get_songs()
         return random.choice(songs.keys())
