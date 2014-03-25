ba8a7e01735870d7a1eb9cb200296d01e6d2c150
George Lifchits <george.lifchits@gmail.com>
2014-03-25 11:58:21 -0400

    like recently played song

--- client/js/controllers/recentlyplayedctrl.js
+++ client/js/controllers/recentlyplayedctrl.js
@@ -2,17 +2,15 @@ angular.module('riseApp.controllers').controller("RecentlyPlayedCtrl", function(
     var songsRef = new Firebase("https://shining-fire-6877.firebaseio.com/songs");
     var recentRef = new Firebase("https://shining-fire-6877.firebaseio.com/songs");
 
-    $scope.recentSongs = [];
+    // bind firebase recent songs to the scope
+    $scope.recentSongs = $firebase(recentRef);
 
-    recentRef.on('value', function(snapshot) {
-        recentQueue = snapshot.val();
-        $scope.recentSongs = [];
+    songs = $firebase(songsRef);
 
-        for (var songId in recentQueue) {
-            songsRef.child(songId).on('value', function(snap) {
-                song = snap.val();
-                $scope.recentSongs.push(song);
-            });
-        };
-    });
+    $scope.likeSong = function(songId) {
+        song = songs.$child(songId);
+        song.$update({likes: song.likes+1});
+    };
+
+
 });
