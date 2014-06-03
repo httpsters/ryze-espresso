5253c6aec8824384be1dccfe904ceb9582a37808
George Lifchits <george.lifchits@gmail.com>
2014-06-03 13:52:00 -0400

    email logging for errors

--- server/main.py
+++ server/main.py
@@ -1,6 +1,24 @@
 from queue import SongQueue
 import time
 import threading
+import logging
+from tlshandler import TlsSMTPHandler
+import config
+import sys
+
+ADMINS = ['george.lifchits@gmail.com']
+
+smtp_handler = TlsSMTPHandler(
+    mailhost=("smtp.gmail.com", 587),
+    fromaddr="george.lifchits@gmail.com",
+    toaddrs=ADMINS,
+    subject=u"ryze-espresso error!",
+    credentials=(config.gmail.username, config.gmail.password)
+)
+smtp_handler.setLevel(logging.ERROR)
+logger = logging.getLogger()
+logger.addHandler(smtp_handler)
+
 
 def start_song():
     print "starting a new song"
@@ -8,16 +26,20 @@ def start_song():
     song_id = q.get_current_song()
     song = q.lookup(song_id)
     song_length = int(song.get('duration', 0) / 1000.0)
-    print "song %s playing. waiting %d seconds for next song" % \
-            (song_id, song_length)
+    logging.info("song %s playing. waiting %d seconds for next song" % \
+            (song_id, song_length))
     threading.Timer(song_length, start_song).start()
 
 if __name__ == '__main__':
-    q = SongQueue()
-    start_song()
-    start = time.time()
-    while True:
-        if time.time() - start > 3:
-            print time.ctime()
-            start = time.time()
+    try:
+        q = SongQueue()
+        start_song()
+        start = time.time()
+        1/0
+        while True:
+            if time.time() - start > 3:
+                print time.ctime()
+                start = time.time()
+    except Exception as e:
+        logger.error(e)
 
