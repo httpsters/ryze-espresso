b9099f1f675912b74ca788b127fe7c476cc60d7a
George Lifchits <george.lifchits@gmail.com>
2014-08-18 20:04:16 -0400

    properly use dev url

--- server/database.py
+++ server/database.py
@@ -1,7 +1,7 @@
 from firebase import firebase
 
 def _get_firebase():
-    url = 'https://shining-fire-6877.firebaseio.com/dev'
+    url = 'https://shining-fire-6877.firebaseio.com/dev/'
     return firebase.FirebaseApplication(url, None)
 
 firebase = _get_firebase()
