61d503cffdf17ca6ad3257647266c011bd7a2dcf
George Lifchits <george.lifchits@gmail.com>
2014-08-20 19:50:19 -0400

    use config

--- server/database.py
+++ server/database.py
@@ -1,7 +1,7 @@
+import config
 from firebase import firebase
 
 def _get_firebase():
-    url = 'https://shining-fire-6877.firebaseio.com/dev/'
-    return firebase.FirebaseApplication(url, None)
+    return firebase.FirebaseApplication(config.firebase.url, None)
 
 firebase = _get_firebase()
